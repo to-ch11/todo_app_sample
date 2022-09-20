527426fde4f9db21d668910760a375efe1322fd3
EC2 Default User <ec2-user@ip-172-31-29-120.ap-northeast-1.compute.internal>
2022-09-20 15:26:57 +0000

    layout編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
-  root "top#index"  
+  devise_for :users, controllers: {
+  registrations: 'users/registrations',
+  sessions: 'users/sessions'
+  }
+  root "top#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   resources :tasks do
