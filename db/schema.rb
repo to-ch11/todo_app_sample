d776c9fac95f11008423d6d9ae68714556b34b2c
EC2 Default User <ec2-user@ip-172-31-29-120.ap-northeast-1.compute.internal>
2022-09-21 21:30:03 +0000

    ログイン・ログアウト遷移編集

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_140739) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index "\"reset_password_token\"", name: "index_users_on_reset_password_token", unique: true
